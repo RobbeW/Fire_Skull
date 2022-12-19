c7b5d6a584383f13acf4d084d3a45eb85760cffd
Robbe Wulgaert <59528716+RobbeW@users.noreply.github.com>
2022-12-19 10:44:08 +0100

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,93 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "myImage":return img`
+........fffffff........
+......fffccccffff......
+.....fcccccccccfff.....
+....fccccccccccccff....
+....ccccccccccccccf....
+...ccccccbcccfcccccf...
+...ccccb..bccfccccbff..
+....ccc....bccfbcbbff..
+.....cb....cccffbbbff..
+.....ccb..bcccccfffff..
+....ffccbbccccccccfff..
+...ff.ccccccccccccfff..
+...f..cccccfccccccff...
+...ccccccccf..fffff....
+...bcccbccff.fcff......
+....b.bfb....fcf.......
+......fff....ccf.......
+......ff....fcc........
+......ff....bc.........
+.....bcb..bccf.........
+.....cccbcccf..........
+......cccccf...........
+.......fff.............
+`;
+            case "image2":
+            case "myImage0":return img`
+.....fffffff.....
+...ffcbbcbbcff...
+..fcbdd1d1ddbcf..
+.fcbddddd11ddbcf.
+.fbbbd111d1ddbbf.
+fcbbdd11111dbbbcf
+fcbbddd111dddbbcf
+fcbbdddddddddbbcf
+fccbbdddddddbbccf
+fccbdddddddddbccf
+fcbd11ddddd11dbcf
+fbcfffcbbbcfffcbf
+fbfffffbdbfffffbf
+fbffffcdddcffffbf
+fbcffbbcfcbbffcbf
+fcbdddbfffbdddbcf
+.ffcbbbfffbbbcff.
+...ffcbcbcbcff...
+.....fbbdbbf.....
+.....fffffff.....
+`;
+            case "image3":
+            case "myImage1":return img`
+2 2 2 2 2 2 2 2 4 2 2 4 2 2 2 2 
+2 4 2 4 2 2 2 5 4 4 2 2 2 4 2 4 
+2 5 2 4 4 4 5 5 2 4 2 c 2 5 2 c 
+2 5 5 4 2 4 5 c 2 2 2 c 5 4 2 c 
+2 4 4 5 2 4 4 c 2 4 4 c 5 4 2 b 
+5 4 4 c c 4 4 b c 2 4 c 4 4 c c 
+b 2 5 c b 5 4 c c 2 5 b 4 2 2 b 
+b 5 4 c c 5 4 b c c 5 c 2 2 2 b 
+b c 2 c b 2 4 b b 2 4 b b 2 4 c 
+c c 4 b b b 2 b c 5 2 b b 2 5 c 
+c d 5 4 b d b b b b b d b 4 2 b 
+c b 2 4 b b 2 b b d b b b 5 b b 
+b b b 5 b d b d d 2 4 b d 5 b b 
+b d b b d d d b b b 5 d d b b b 
+d d d d d b b d d b b d d d b d 
+d b b d d b d d d d d b d b d d 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
